f937a84cf5d1f2061fbe938132baca99a4f2f588
Carlos Afonso <afonso.carlos.braga@gmail.com>
2022-08-08 10:03:26 -0300

    Add Request Params parsing

--- app/Lib/Request.php
+++ app/Lib/Request.php
@@ -5,4 +5,63 @@ namespace App\Lib;
 
 class Request
 {
+    private string $method;
+    private array $params;
+    private array $segments;
+
+    public function __construct(array $segments = [])
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->params = [];
+        $this->segments = $segments;
+        $this->withParams();
+    }
+
+    private function withParams(): void
+    {
+        if ($this->method === 'GET') {
+            $inputType = INPUT_GET;
+            $params = $_GET;
+        } elseif ($this->method === 'POST') {
+            $inputType = INPUT_POST;
+            $params = $_POST;
+        }
+
+        foreach ($params as $key => $value) {
+            $this->params[$key] = filter_input($inputType, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+
+        $content = file_get_contents('php://input');
+        $this->params = array_merge($params, json_decode($content, true) ?? []);
+    }
+
+    /**
+     * Getter for method
+     *
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * Getter for params
+     *
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    /**
+     * Getter for segments
+     *
+     * @return array
+     */
+    public function getSegments(): array
+    {
+        return $this->segments;
+    }
 }
--- test/Lib/RequestTest.php
+++ test/Lib/RequestTest.php
@@ -10,8 +10,9 @@ class RequestTest extends TestCase
 {
     public function testRequestSegments()
     {
-        $request = new Request(params: [1]);
-        $this->assertEquals([1], $request->params);
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $request = new Request(segments: [1]);
+        $this->assertEquals([1], $request->getSegments());
     }
 
     public function testRequestGetMethod()
@@ -27,4 +28,21 @@ class RequestTest extends TestCase
         $request = new Request();
         $this->assertEquals('POST', $request->getMethod());
     }
+
+    public function testRequestGetQueryParams()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_GET = ['id' => 1, 'name' => 'John'];
+        $request = new Request();
+        $this->assertEquals(['id' => 1, 'name' => 'John'], $request->getParams());
+    }
+
+    public function testRequestPostBodyParams()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_POST = ['id' => 1, 'name' => 'John', 'age' => '30'];
+        $request = new Request();
+        $this->assertEquals(['id' => 1, 'name' => 'John', 'age' => '30'], $request->getParams());
+    }
+
 }
